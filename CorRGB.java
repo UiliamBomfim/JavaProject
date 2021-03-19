29d1c611b576e5c47644cb2860b5737b2aa1bea8
Uiliam Bomfim <wf3@outlook.com.br>
2021-03-19 18:20:35 -0300

    add commit

---
+++ CorRGB.java
@@ -0,0 +1,73 @@
+class CorRGB
+{
+    private int valorRed;
+    private int valorGreen;
+    private int valorBlue;
+
+    
+
+    //construtor cor RGB
+    CorRGB(int red, int green, int blue){
+        try {
+            if (red <= 0 || red >= 255 || green <= 0 || green >= 255 || blue <= 0 || blue >= 255  )
+
+                throw new Exception("Número inválido");
+            else {
+                this.setValorRed(red);
+                this.setValorGreen(green);
+                this.setValorBlue(blue);}
+            } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+     //CorRGB(){}
+     //CorRGB(Cor preta){}
+
+
+
+    public int getValorRed() {
+        return valorRed;
+    }
+
+    public void setValorRed(int valorRed) {
+        this.valorRed = valorRed;
+    }
+
+    public int getValorGreen() {
+        return valorGreen;
+    }
+
+    public void setValorGreen(int valorGreen) {
+        this.valorGreen = valorGreen;
+    }
+
+    public int getValorBlue() {
+        return valorBlue;
+    }
+
+    public void setValorBlue(int valorBlue) {
+        this.valorBlue = valorBlue;
+    }
+
+    //public double getLuminosidade(){    }
+
+   
+
+    @Override
+    public String toString(){
+        String s = "";
+
+
+            s +=    "valor de vermelho: "+ valorRed +"\n"+
+                    "valor de verde: "+ valorGreen +"\n"+
+                    "valor de azul: " + valorBlue+"\n";
+        return s;
+        }
+
+
+
+
+}
+
